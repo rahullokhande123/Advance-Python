21f71b1cb7cb1544a5b679a8206a79b5efa2ff7c
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-18 16:46:27 +0530

    File_Handling_Basic

--- File_Handling/P1.py
+++ File_Handling/P1.py
@@ -11,7 +11,18 @@
 # print(f.writable())
 # ===================================================
 
-f=open('n2.txt','x')
+# f=open('n2.txt','x')
+# print(f.name)
+# print(f.mode)
+# print(f.encoding)
+# print(f.readable())
+# print(f.writable())
+# print(f.closed)
+# f.close()
+# print(f.closed)
+# ===================================================
+
+f=open('n5.txt','x+')
 print(f.name)
 print(f.mode)
 print(f.encoding)
