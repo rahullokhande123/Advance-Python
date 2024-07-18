4cf2164571793ca0629d94e3aa0131fe76240c3e
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-18 16:41:40 +0530

    File_Handling_Basic

--- File_Handling/P1.py
+++ File_Handling/P1.py
@@ -1,16 +1,24 @@
-f=open('new.txt','x')
-f=open('new1.txt','w')
-f=open('new2.txt','a')
-f=open('new4.txt','x')
-# print(f.read())
+# f=open('new.txt','x')
+# f=open('new1.txt','w')
+# f=open('new2.txt','a')
+# f=open('new4.txt','x')
+# # print(f.read())
+# print(f.readable())
+# print(f.writable())
+# print(f.closed)
+# f.close()
+# print(f.closed)
+# print(f.writable())
+# ===================================================
+
+f=open('n2.txt','x')
+print(f.name)
+print(f.mode)
+print(f.encoding)
 print(f.readable())
 print(f.writable())
 print(f.closed)
 f.close()
 print(f.closed)
-print(f.writable())
-# ===================================================
-
-
 
 
