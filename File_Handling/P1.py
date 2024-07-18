8f97befb7109a3756dbc8eee150c05307dcbb9b0
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-18 16:48:35 +0530

    File_Handling_Basic

--- File_Handling/P1.py
+++ File_Handling/P1.py
@@ -22,14 +22,20 @@
 # print(f.closed)
 # ===================================================
 
-f=open('n5.txt','x+')
-print(f.name)
-print(f.mode)
-print(f.encoding)
-print(f.readable())
-print(f.writable())
-print(f.closed)
-f.close()
-print(f.closed)
+# f=open('n5.txt','x+')
+# print(f.name)
+# print(f.mode)
+# print(f.encoding)
+# print(f.readable())
+# print(f.writable())
+# print(f.closed)
+# f.close()
+# print(f.closed)
+# ===================================================
 
+f=open('n6.txt','w')
+f.write("Hello I am Rahul Lokhade\nI am Fullstack Developer\n")
+data=('This is C++ Timing 10:30 to 11:30')
+f.writelines(data)
+f.close()
 
