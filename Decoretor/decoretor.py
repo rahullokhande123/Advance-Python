51e17b6279cb9245208934e1d82c4ed61edeff1f
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-24 15:43:06 +0530

    Decorator

--- Decoretor/decoretor.py
+++ Decoretor/decoretor.py
@@ -11,3 +11,17 @@ var1=outer_fun(main_fun)
 var2=var1(5,6)
 print(var2)
 
+#============================================
+# Right Way :-
+def outer_fun(fun):
+    def inner_fun(x,y):
+        x=x+10
+        y=y+10
+        print("main function call")
+        return fun(x,y)
+    return inner_fun
+@outer_fun
+def main_fun(x,y):
+    return x+y
+x=main_fun(5,6)
+print(x)
