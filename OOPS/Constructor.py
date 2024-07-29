988d527450bae78ca843224ffcae1819776355ec
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-29 22:33:19 +0530

    Constructor

--- OOPS/Constructor.py
+++ OOPS/Constructor.py
@@ -23,3 +23,21 @@ obj=Student()
 
 print(obj.x)
 obj.display()    # () lagana must be recurged
+
+# ============= Self And Current Class Objects Same Address ===============
+class Student:
+    x=10;
+    y=20;
+    def __init__(self):
+        print("This is Construnctor")
+        print(id(self))
+    @staticmethod
+    def display():
+        print("This is Display")
+obj=Student()
+
+print(obj.x)
+print(id(obj))
+obj.display()
+obj2=Student()
+print(id(obj2))
