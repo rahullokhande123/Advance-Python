fa117b17f6549c68475557761e15c6909f232f11
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-30 20:57:28 +0530

    Variables

---
+++ OOPS/Variables.py
@@ -0,0 +1,18 @@
+# 1) ============ Instance V. Declaration through Constructor ==============
+class Student:
+    def __init__(self,name,age):
+        self.n1=name
+        self.n2=age
+    def display(self):
+        print("Name =",self.n1)
+        print("Age =",self.n2)
+obj=Student("Rahul",24)
+obj.display()
+obj1=Student("Arun",24)
+obj1.display()
+
+
+
+
+
+
