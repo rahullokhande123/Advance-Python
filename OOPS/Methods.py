1bf8dc393194260c83e65fac9e188299d5c9dbb3
Rahul Lokhande <rlokhande06244@gmail.com>
2024-08-02 21:51:15 +0530

    Class-Method

--- OOPS/Methods.py
+++ OOPS/Methods.py
@@ -16,4 +16,21 @@ obj=Student("Rahul",101)
 obj.show()
 obj.display()
 
+# Instance Methode Ko Bhi jab kisi method me call krte hai to self ki helf se hi hota h
+# Or Method Ko Class Ke Baher Call krne ke liye obj.ka use krke 
 
+# ================ Class Method =====================
+
+class Book:
+    price=1000
+    def datails(self,author_name,author_city):
+        print("Name=",author_name)
+        print("City=",author_city)
+        print("price=",Book.price)
+    @classmethod
+    def update_price(cls,updated_price):
+        cls.price=updated_price
+obj=Book()
+obj.datails("Neeraj","BHopal")
+obj.update_price(2000)
+obj.datails("Rahul","Bhopal")
