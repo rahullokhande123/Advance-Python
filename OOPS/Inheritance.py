6a5d469987859ed0d548edc6d4ae0e46af99a534
Rahul Lokhande <rlokhande06244@gmail.com>
2024-08-07 15:32:44 +0530

    Inheritance

--- OOPS/Inheritance.py
+++ OOPS/Inheritance.py
@@ -33,3 +33,24 @@ obj.show1()
 obj.show()
 obj.display()
 print(obj.city)
+
+# ===================== Multiple Inheritance =======================
+
+class P:
+    city="Bhopal"
+    def display(self):
+        print("This is from Display P")
+class C:      
+        
+    def display(self):
+        print("This is from Display C")
+class C1(P,C):
+    city1=P.city
+    def show1(self):
+        self.display()
+        
+obj=C1()
+print(obj.city1)
+obj.display()
+print(obj.city)
+obj.show1()
