708d80f942836ad2fe1aaacb8ecac6db8ecd64d6
Rahul Lokhande <rlokhande06244@gmail.com>
2024-08-07 15:31:34 +0530

    Inheritance

--- OOPS/Inheritance.py
+++ OOPS/Inheritance.py
@@ -12,3 +12,24 @@ obj=C()
 obj.show()
 print(obj.city,obj.display())
 
+# ===================== Multilevel Inheritance =======================
+
+class P:
+    city="Bhopal"
+    def display(self):
+        print("This is from Display")
+class C(P):
+    def show(self):
+        print("Show Section")
+        print("Parent City=",P.city)
+class C1(C):
+    city1=P.city
+    def show1(self):
+        self.display()
+        self.show()
+obj=C1()
+print(obj.city1)
+obj.show1()
+obj.show()
+obj.display()
+print(obj.city)
