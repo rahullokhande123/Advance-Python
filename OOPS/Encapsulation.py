5ae1216129bb1055788015853baa4e42addc8a49
Rahul Lokhande <rlokhande06244@gmail.com>
2024-08-08 23:58:22 +0530

    Encapsulation

--- OOPS/Encapsulation.py
+++ OOPS/Encapsulation.py
@@ -12,3 +12,16 @@ obj=P2()
 obj.show()
 print(obj._name)
 
+# ========================== Private Acces Spacifier =======================
+
+# class P1:                         # Ye Child Class me Access Nhi Krta Hai.
+#     _name="Rahul"
+#     def display(self):
+#         print(P1._name)
+# class P2(P1):                     # Private -> ( __ )
+#     name=P1._name
+#     def show(self):
+#         print("NAME=",P2.name)
+# obj=P2()
+# obj.show()
+# print(obj._name)
