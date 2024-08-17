eee28f2e00fb3faf96e772fe985d1afde6883fe5
Rahul Lokhande <rlokhande06244@gmail.com>
2024-08-17 12:18:14 +0530

    Decoretor

--- decoretor.py
+++ decoretor.py
@@ -1,30 +1,33 @@
-# def outer_fun(fun):
-#     def inner_fun(x,y):
-#         x=x+10
-#         y=y+10
-#         print("main function call")
-#         return fun(x,y)
-#     return inner_fun
-# def main_fun(x,y):
-#     return x+y
-# var1=outer_fun(main_fun)
-# var2=var1(5,6)
-# print(var2)
+def outer_fun(fun):
+    def inner_fun(x,y):
+        x=x+10
+        y=y+10
+        print("main function call")
+        return fun(x,y)
+    return inner_fun
+def main_fun(x,y):
+    return x+y
+var1=outer_fun(main_fun)
+var2=var1(5,6)
+print(var2)
 
-# #============================================
-# # Right Way :-
-# def outer_fun(fun):
-#     def inner_fun(x,y):
-#         x=x+10
-#         y=y+10
-#         return fun(x,y)
-#     return inner_fun
-# @outer_fun
-# def main_fun(x,y):
-#     return x+y
-# x=main_fun(5,6)
-# print(x)
+print("============================")
+#============================================
+# Right Way :-
+def outer_fun(fun):
+    def inner_fun(x,y):
+        x=x+10
+        y=y+10
+        return fun(x,y)
+    return inner_fun
+@outer_fun
+def main_fun(x,y):
+    return x+y
+x=main_fun(5,6)
+print(x)
 
+print("============================")
+# ==============================================
 
 def outer(fun):
     def inner(a,b):
@@ -32,7 +35,7 @@ def outer(fun):
         print("Value of a:",a)
         print("Value of b:",b)
         modify=a*b
-        print("Inner Function:",modify)
+        print("Multiply By Inner Function:",modify)
     return inner
 
 @outer
@@ -40,4 +43,19 @@ def add(a,b):
     x=a+b
     print(x)
 add(5,5)
-    
+
+print("============================")
+# =====================================================
+
+def reward(fun):
+    def reward_gift():
+        return fun()
+        print("Congratulation You Win a Speaker")
+    return reward_gift
+
+@reward   
+def mobile():
+    gift="Buy iphone 16 Pro "
+    print(gift)
+x=mobile()
+print(x)
